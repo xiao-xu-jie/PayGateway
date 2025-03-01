d674e6b914b2c9e4dbe01d72fb0020a6d15b5919
xujie <3285503539@qq.com>
2025-03-01 21:30:22 +0800

    feat(outboxMessage): 采用乐观锁实现并发控制

--- xiaoxupay-order/order-infra/src/main/java/com/xujie/infra/entity/OutboxMessage.java
+++ xiaoxupay-order/order-infra/src/main/java/com/xujie/infra/entity/OutboxMessage.java
@@ -1,9 +1,6 @@
 package com.xujie.infra.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -81,6 +78,7 @@ public class OutboxMessage {
      * 重试次数
      */
     @TableField(value = "retry_count")
+    @Version
     private Integer retryCount;
 
     public static final String COL_ID = "id";
